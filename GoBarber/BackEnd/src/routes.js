b59b314d838d51ee839cf8c4848bb11aee448eee
pdNN <pedrolucho02@gmail.com>
2020-03-28 21:02:01 -0300

    att

--- GoBarber/BackEnd/src/routes.js
+++ GoBarber/BackEnd/src/routes.js
@@ -4,6 +4,8 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import multerConfig from './config/multer';
 
@@ -18,6 +20,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(AuthMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
